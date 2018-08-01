0edf803d8d641ca9203cfd3745817dd135847102
Enzo Pietropaolo <peter@petertools.com>
2018-08-01 09:29:34 +0200

    Added MIDIKey control.

---
+++ src/JuliusSweetland.OptiKey/UI/Controls/MIDIKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using JuliusSweetland.OptiKey.Enums;
+using JuliusSweetland.OptiKey.Extensions;
+using JuliusSweetland.OptiKey.Models;
+using JuliusSweetland.OptiKey.Properties;
+using JuliusSweetland.OptiKey.UI.Utilities;
+using JuliusSweetland.OptiKey.UI.ViewModels;
+
+namespace JuliusSweetland.OptiKey.UI.Controls
+{
+    public class MIDIKey : Key
+    {
+
+    }
+}
